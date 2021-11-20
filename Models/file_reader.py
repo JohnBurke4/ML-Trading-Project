c992b00c7cd49d2b217e932cd11c9a0a88d93df2
John Burke <burkejohn1999@gmail.com>
2021-11-20 20:07:58 +0000

    Removing header check from CSV File

--- Models/file_reader.py
+++ Models/file_reader.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
+
 def read_regression():
-    df = pd.read_csv(f"../ParsedData/Data.csv")
-    X = df.iloc[:,0:66]
+    df = pd.read_csv(f"../ParsedData/Data.csv", header=None)
+    X = df.iloc[:, 0:66]
     y = df.iloc[:, 66]
-    return (X, y)    
+    return (X, y)
+
 
 def read_classifier():
-    df = pd.read_csv(f"../ParsedData/DataClassifier.csv")
-    X = df.iloc[:,0:66]
+    df = pd.read_csv(f"../ParsedData/DataClassifier.csv", header=None)
+    X = df.iloc[:, 0:66]
     y = df.iloc[:, 66]
     return (X, y)
--- Models/logistic_regression.py
+++ Models/logistic_regression.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 
 (X, y) = read_classifier()
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-model = LogisticRegression(penalty='none',solver='lbfgs', max_iter=1000).fit(X_train, y_train)
+model = LogisticRegression(
+    penalty='none', solver='lbfgs', max_iter=1000).fit(X_train, y_train)
 dummy = DummyClassifier().fit(X_train, y_train)
 print("LogisticRegression: ", model.score(X_test, y_test))
 print("DummyClassifier: ", dummy.score(X_test, y_test))
