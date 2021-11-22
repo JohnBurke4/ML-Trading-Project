77eb8cb317cdb5a428f8a705f249eb97b7f4d084
Daniel Nugent <55405053+DanielNugent@users.noreply.github.com>
2021-11-22 15:23:52 +0000

    added KFold to logistic

--- Models/logistic_regression.py
+++ Models/logistic_regression.py
@@ -1,23 +1,17 @@
 from file_reader import read_classifier
+from utils import KFold_validate
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import KFold
 filename = 'dataClassifier2Days.csv'
 (X, y) = read_classifier(filename=filename)
 poly = PolynomialFeatures(2)
 X = poly.fit_transform(X)
-kf = KFold(n_splits=5)
-kf.get_n_splits(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
 model = LogisticRegression(
-    penalty='none', solver='lbfgs', max_iter=1000).fit(X_train, y_train)
-dummy = DummyClassifier().fit(X_train, y_train)
-print("LogisticRegression: ", model.score(X_test, y_test))
-print("DummyClassifier: ", dummy.score(X_test, y_test))
-for train_index, test_index in kf.split(X):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    penalty='none', solver='lbfgs', max_iter=1500)
+dummy = DummyClassifier()
+(mean_auc, std_dev) = KFold_validate(model, dummy, X, y)
+print("Logistic AUC: ", mean_auc[0], " with deviation: ", std_dev[0])
+print("Dummy AUC: ", mean_auc[1], " with deviation: ", std_dev[1])
