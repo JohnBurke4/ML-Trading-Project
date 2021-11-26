22034cef2bbac2edf5e177a6d537557b67adcdfb
moreplatesmoredates <nugentd2@tcd.ie>
2021-11-26 14:48:08 +0000

    fixed simulator func

--- Models/file_reader.py
+++ Models/file_reader.py
@@ -2,29 +2,36 @@ import pandas as pd
 
 
 def get_filename(type):
+    n_stocks = 1
     size = input("Enter data size (S/M/L): ")
     if (size.lower() == "s"):
         size = "Small"
     elif (size.lower() == "m"):
         size = "Medium"
-    else: size = "Large"
-    return f'{type}{size}Data.csv'
+        n_stocks = 10
+    else: 
+        size = "Large"
+        n_stocks = 100
+
+    return (f'{type}{size}Data.csv', n_stocks)
 
 def read_regression(n_days=14):
-    df = pd.read_csv("../ParsedData/{}".format(get_filename("linear")), header=None)
+    (filename, n_stocks) = get_filename("linear")
+    df = pd.read_csv("../ParsedData/{}".format(filename), header=None)
     cols = df.shape[1]
     columns_to_remove = (14-n_days) * 13
     X = df.iloc[:, 0:(cols-2) - columns_to_remove]
     y = df.iloc[:, (cols-1)]
     print(y)
-    return (X, y)
+    return (X, y, n_stocks)
 
 
 def read_classifier(n_days=14):
-    df = pd.read_csv("../ParsedData/{}".format(get_filename("classifier")), header=None)
+    (filename, n_stocks) = get_filename("classifier")
+    df = pd.read_csv("../ParsedData/{}".format(filename), header=None)
     cols = df.shape[1]
     columns_to_remove = (14-n_days) * 13
     X = df.iloc[:, 0:((cols-1) - columns_to_remove)]
     y = df.iloc[:, (cols-1)]
-    return (X, y)
+    return (X, y, n_stocks)
 
